380525e1ce511b62ca3c9e049043b6ff7e0220c1
Muzamil Khan <88248071+m-zamil@users.noreply.github.com>
2021-11-30 09:19:16 +0500

    landing page mobile

--- js/index.js
+++ js/index.js
@@ -40,8 +40,19 @@ $(document).ready(function () {
     dots: true,
   });
   $("#grow-slider").owlCarousel({
-    items: 3,
     navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+    center: false,
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+        stagePadding: 20,
+      },
+      768: {
+        items: 3,
+        stagePadding: 0,
+      },
+    },
     loop: false,
     margin: 10,
     nav: true,
@@ -49,29 +60,61 @@ $(document).ready(function () {
   });
 
   $("#highlight_slider").owlCarousel({
-    items: 2,
-    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
     loop: false,
     margin: 20,
-    nav: true,
     dots: true,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 30,
+        nav: false,
+      },
+      768: {
+        items: 2,
+        nav: true,
+        navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+      },
+    },
   });
 
   $("#review-slider").owlCarousel({
     items: 3,
-    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+
     stagePadding: 150,
     center: true,
     loop: true,
     margin: 10,
-    nav: true,
     dots: true,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 30,
+        loop: false,
+        nav: false,
+      },
+      768: {
+        items: 3,
+        nav: true,
+        loop: true,
+        navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+      },
+    },
   });
 
   $(".owl-carousel").owlCarousel({
     items: 1,
-    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+
     loop: false,
+    responsive: {
+      0: {
+        nav: false,
+        stagePadding: 20,
+      },
+      768: {
+        navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        stagePadding: 0,
+      },
+    },
     margin: 10,
     nav: true,
     dots: true,
